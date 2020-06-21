729013438d300ccf4a201db450572c600c15b9f6
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 07:20:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,11 +16,20 @@ class Song
     @@all
   end
    
-   def self.new_by_filename(filename)
-    song = self.new
-    song.name = filename.split(" - ")[1]
+ #  def self.new_by_filename(filename)
+  #  song = self.new
+   # song.name = filename.split(" - ")[1]
+    #song
+#  end
+ 
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
     song
   end
+end
+ 
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
