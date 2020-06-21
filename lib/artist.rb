e379f9ca8870bed3244db7d15efcbdce5db9351e
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 06:29:01 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+
+attr_accessor :name 
+@@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+   def add_song(song)
+    song.artist = self
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+
+end 
